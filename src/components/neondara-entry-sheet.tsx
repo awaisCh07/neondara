3c70a9340276c3ad6bf23b5a47db83662e10dc99
Awais Chaudhry <chaudhryawais.533@gmail.com>
2025-07-11 19:03:13 +0000

    no, not working on mobile

--- src/components/neondara-entry-sheet.tsx
+++ src/components/neondara-entry-sheet.tsx
@@ -276,7 +276,7 @@ export function NeondaraEntrySheet({ isOpen, onOpenChange, onAddEntry, onUpdateE
             render={({ field }) => (
                 <FormItem className="flex flex-col">
                 <FormLabel>{t('dateOfOccasion')} *</FormLabel>
-                <Popover modal={false}>
+                <Popover>
                     <PopoverTrigger asChild>
                     <FormControl>
                         <Button
@@ -295,17 +295,19 @@ export function NeondaraEntrySheet({ isOpen, onOpenChange, onAddEntry, onUpdateE
                         </Button>
                     </FormControl>
                     </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                      <Calendar
-                          mode="single"
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                          }
-                          initialFocus
-                      />
-                    </PopoverContent>
+                    <PopoverPortal>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                            mode="single"
+                            selected={field.value}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                            date > new Date() || date < new Date("1900-01-01")
+                            }
+                            initialFocus
+                        />
+                      </PopoverContent>
+                    </PopoverPortal>
                 </Popover>
                 <FormMessage />
                 </FormItem>
@@ -475,5 +477,3 @@ export function NeondaraEntrySheet({ isOpen, onOpenChange, onAddEntry, onUpdateE
     </Sheet>
   )
 }
-
-    
